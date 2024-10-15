ed82057431a77e1850d8bcfb58a18ee18182308b
furqan4545 <furqan.ali9500@gmail.com>
2024-10-15 23:45:43 +0900

    launch 1.0

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
